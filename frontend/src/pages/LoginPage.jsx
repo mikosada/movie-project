3addc05ac25b25d01ec17b27bfa4d839ae051e68
mikosada <mikosaddamitha123@gmail.com>
2024-09-14 17:10:38 +0700

    update indexcss scroll

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -6,7 +6,7 @@ const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const { login } = useAuthStore();
+  const { login, isLoggingIn } = useAuthStore();
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -60,8 +60,11 @@ const LoginPage = () => {
               />
             </div>
 
-            <button className="w-full py-2 bg-red-600 text-white font-semibold rounded-md hover:bg-red-700 ">
-              Sign Up
+            <button
+              className="w-full py-2 bg-red-600 text-white font-semibold rounded-md hover:bg-red-700"
+              disabled={isLoggingIn}
+            >
+              {isLoggingIn ? "Loading. . ." : "Login"}
             </button>
           </form>
           <div className="text-center text-gray-400">
--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -9,7 +9,7 @@ const SignupPage = () => {
   const [email, setEmail] = useState(emailValue || "");
   const [password, setPassword] = useState("");
 
-  const { signup } = useAuthStore();
+  const { signup, isSigningUp } = useAuthStore();
 
   const handleSignUp = (e) => {
     e.preventDefault();
@@ -80,8 +80,11 @@ const SignupPage = () => {
               />
             </div>
 
-            <button className="w-full py-2 bg-red-600 text-white font-semibold rounded-md hover:bg-red-700 ">
-              Sign Up
+            <button
+              className="w-full py-2 bg-red-600 text-white font-semibold rounded-md hover:bg-red-700"
+              disabled={isSigningUp}
+            >
+              {isSigningUp ? "Loading. . ." : "Sign Up"}
             </button>
           </form>
           <div className="text-center text-gray-400">
